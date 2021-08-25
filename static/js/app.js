b27af7f1ac326fdf6293470f244175b2185a467f
kennethcandersen <kcab98@gmail.com>
2021-08-24 22:11:50 -0500

    Demo page with color scheme and boxplots

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,7 @@
 
 
-d3.selectAll("input").on("change",runEnter);
+d3.selectAll("input").on("change", runEnter);
+d3.selectAll("select").on("change", runEnter);
 
 plotWidth = 400
 plotHeight = 250
@@ -19,6 +20,7 @@ function runEnter(demoImput1) {
         var distHome = [];
         var jobSat = [];
         var workYears = [];
+        var marStatus = []
         var monInc = []
 
         ibmData.forEach(d => {
@@ -26,24 +28,35 @@ function runEnter(demoImput1) {
             distHome.push(+d.DistanceFromHome);
             jobSat.push(+d.JobSatisfaction);
             workYears.push(+d.TotalWorkingYears);
+            marStatus.push(d.MaritalStatus);
             monInc.push(+d.MonthlyIncome);
 
         });
 
-        // 1. TRAINING TIME PLOT
-        var trace = {
+
+        // 1. TRAINING  PLOT
+        var data = [{
             x: trainTime,
-            type: 'histogram',
-        };
-        var data = [trace];
+            type: 'box',
+            marker: { color: '#43BCCD' },
+            boxmean: 'sd',
+            showticklabels: false
+        }]
+
+        // var data = [trace];
 
         var layout = {
-            title: "Training Time",
+            title: {
+                text: "Training Courses",
+                font: {
+            color: '#43BCCD'
+                }
+        },
             width: plotWidth,
             height: plotHeight,
             margin: {
-                l: 30,
-                r: 10,
+                l: 0,
+                r: 5,
                 b: 40,
                 t: 80,
                 pad: 0
@@ -52,41 +65,34 @@ function runEnter(demoImput1) {
             plot_bgcolor: '#eee',
             bargap: 0.2,
             xaxis: {
-                title: 'Training Days',
-                // linecolor: 'black',
-                // linewidth: 1,
-                // mirror: true
+                title: 'Courses Taken',
             },
-            yaxis: {
-                // linecolor: 'black',
-                // linewidth: 1,
-                // mirror: true
-            },
+
             annotations: [
                 {
-                font: {
-                    color: '#404040'
-                },
-                yref: 'paper',
-                x: demoImput1,
-                y: .8,
-                text: 'Employee Profile',
-                showarrow: true,
-                arrowhead: 7,
+                    font: {
+                        color: '#404040'
+                    },
+                    yref: 'paper',
+                    x: demoImput1,
+                    y: .8,
+                    text: 'Employee Profile',
+                    showarrow: true,
+                    arrowhead: 7,
                 }
             ],
             shapes: [
                 {
-                  type: 'line',
-                  yref: 'paper',
-                  x0: demoImput1,
-                  y0: 0,
-                  x1: demoImput1,
-                  y1: 1,
-                  line: {
-                    color: '#404040',
-                    width: 3
-                  }
+                    type: 'line',
+                    yref: 'paper',
+                    x0: demoImput1,
+                    y0: 0,
+                    x1: demoImput1,
+                    y1: 1,
+                    line: {
+                        color: '#404040',
+                        width: 3
+                    }
                 }],
         };
         var config = { responsive: true }
@@ -96,17 +102,23 @@ function runEnter(demoImput1) {
         // 2. TRAINING TIME PLOT
         var trace = {
             x: distHome,
-            type: 'histogram',
+            type: 'box',
+            marker: { color: '#0D31AE' },
+            boxmean: 'sd'
         };
         var data = [trace];
 
         var layout = {
-            title: "Distance from Home",
+            title: {text: "Distance from Home",
+            font: {
+            color: '#0D31AE',
+            weight: 'bold'},
+            },
             width: plotWidth,
             height: plotHeight,
             margin: {
-                l: 30,
-                r: 10,
+                l: 0,
+                r: 5,
                 b: 40,
                 t: 80,
                 pad: 0
@@ -116,40 +128,33 @@ function runEnter(demoImput1) {
             bargap: 0.2,
             xaxis: {
                 title: 'Distance',
-                // linecolor: 'black',
-                // linewidth: 1,
-                // mirror: true
-            },
-            yaxis: {
-                // linecolor: 'black',
-                // linewidth: 1,
-                // mirror: true
-            },
+                           },
             annotations: [
                 {
-                font: {
-                    color: '#404040'
-                },
-                yref: 'paper',
-                x: demoImput2,
-                y: .8,
-                text: 'Employee Profile',
-                showarrow: true,
-                arrowhead: 7,
+                    font: {
+                        color: '#404040'
+                    },
+                    yref: 'paper',
+                    x: demoImput2,
+                    y: .8,
+                    text: 'Employee Profile',
+                    showarrow: true,
+                    arrowhead: 7,
                 }
             ],
+
             shapes: [
                 {
-                  type: 'line',
-                  yref: 'paper',
-                  x0: demoImput2,
-                  y0: 0,
-                  x1: demoImput2,
-                  y1: 1,
-                  line: {
-                    color: '#404040',
-                    width: 3
-                  }
+                    type: 'line',
+                    yref: 'paper',
+                    x0: demoImput2,
+                    y0: 0,
+                    x1: demoImput2,
+                    y1: 1,
+                    line: {
+                        color: '#404040',
+                        width: 3
+                    }
                 }],
         };
         var config = { responsive: true }
@@ -159,17 +164,23 @@ function runEnter(demoImput1) {
         // 3. JOB SATISFACTION PLOT
         var trace = {
             x: jobSat,
-            type: 'histogram',
+            type: 'box',
+            marker: { color: '#F6C84E' },
+            boxmean: 'sd'
         };
         var data = [trace];
 
         var layout = {
-            title: "Job Satisfaction",
+            title: {
+                text: "Job Satisfaction",
+                font: {
+            color: '#F6C84E'},
+                },
             width: plotWidth,
             height: plotHeight,
             margin: {
-                l: 30,
-                r: 10,
+                l: 0,
+                r: 5,
                 b: 40,
                 t: 80,
                 pad: 0
@@ -179,40 +190,32 @@ function runEnter(demoImput1) {
             bargap: 0.2,
             xaxis: {
                 title: 'Satisfaction Rating',
-                // linecolor: 'black',
-                // linewidth: 1,
-                // mirror: true
-            },
-            yaxis: {
-                // linecolor: 'black',
-                // linewidth: 1,
-                // mirror: true
             },
             annotations: [
                 {
-                font: {
-                    color: '#404040'
-                },
-                yref: 'paper',
-                x: demoImput3,
-                y: .8,
-                text: 'Employee Profile',
-                showarrow: true,
-                arrowhead: 7,
+                    font: {
+                        color: '#404040'
+                    },
+                    yref: 'paper',
+                    x: demoImput3,
+                    y: .8,
+                    text: 'Employee Profile',
+                    showarrow: true,
+                    arrowhead: 7,
                 }
             ],
             shapes: [
                 {
-                  type: 'line',
-                  yref: 'paper',
-                  x0: demoImput3,
-                  y0: 0,
-                  x1: demoImput3,
-                  y1: 1,
-                  line: {
-                    color: '#404040',
-                    width: 3
-                  }
+                    type: 'line',
+                    yref: 'paper',
+                    x0: demoImput3,
+                    y0: 0,
+                    x1: demoImput3,
+                    y1: 1,
+                    line: {
+                        color: '#404040',
+                        width: 3
+                    }
                 }],
         };
         var config = { responsive: true }
@@ -222,17 +225,23 @@ function runEnter(demoImput1) {
         // 4. TOTAL WORKING TIME PLOT
         var trace = {
             x: workYears,
-            type: 'histogram',
+            type: 'box',
+            marker: { color: '#F6461D' },
+            boxmean: 'sd'
         };
         var data = [trace];
 
         var layout = {
-            title: "Total Years Working",
+            title: {
+                text: "Total Years Working",
+                font: {
+            color: '#F6461D'},
+                },
             width: plotWidth,
             height: plotHeight,
             margin: {
-                l: 30,
-                r: 10,
+                l: 0,
+                r: 5,
                 b: 40,
                 t: 80,
                 pad: 0
@@ -242,61 +251,120 @@ function runEnter(demoImput1) {
             bargap: 0.2,
             xaxis: {
                 title: 'Years',
-                // linecolor: 'black',
-                // linewidth: 1,
-                // mirror: true
-            },
-            yaxis: {
-                // linecolor: 'black',
-                // linewidth: 1,
-                // mirror: true
             },
             annotations: [
                 {
-                font: {
-                    color: '#404040'
-                },
-                yref: 'paper',
-                x: demoImput4,
-                y: .8,
-                text: 'Employee Profile',
-                showarrow: true,
-                arrowhead: 7,
+                    font: {
+                        color: '#404040'
+                    },
+                    yref: 'paper',
+                    x: demoImput4,
+                    y: .8,
+                    text: 'Employee Profile',
+                    showarrow: true,
+                    arrowhead: 7,
                 }
             ],
             shapes: [
                 {
-                  type: 'line',
-                  yref: 'paper',
-                  x0: demoImput4,
-                  y0: 0,
-                  x1: demoImput4,
-                  y1: 1,
-                  line: {
-                    color: '#404040',
-                    width: 3
-                  }
+                    type: 'line',
+                    yref: 'paper',
+                    x0: demoImput4,
+                    y0: 0,
+                    x1: demoImput4,
+                    y1: 1,
+                    line: {
+                        color: '#404040',
+                        width: 3
+                    }
                 }],
         };
         var config = { responsive: true }
 
         Plotly.newPlot('demoChart4', data, layout, config);
 
-
-        // 6. MONTHLY INCOME PLOT
+        // 5. Marital Status
         var trace = {
-            x: monInc,
-            type: 'histogram',
+            x: marStatus,
+            type: 'box',
+            marker: { color: '#AFBAF4' },
+            boxmean: 'sd'
         };
         var data = [trace];
 
         var layout = {
-            title: "Monthly Income",
+            title: {text: "Marital Status",
+            font: {
+            color: '#AFBAF4'},
+            },
             width: plotWidth,
             height: plotHeight,
             margin: {
-                l: 30,
-                r: 10,
+                l: 0,
+                r: 5,
+                b: 40,
+                t: 80,
+                pad: 0
+            },
+            paper_bgcolor: '#eee',
+            plot_bgcolor: '#eee',
+            bargap: 0.2,
+            xaxis: {
+                title: 'Status',
+            },
+            annotations: [
+                {
+                    font: {
+                        color: '#404040'
+                    },
+                    yref: 'paper',
+                    x: demoImput5,
+                    y: .8,
+                    text: 'Employee Profile',
+                    showarrow: true,
+                    arrowhead: 7,
+                }
+            ],
+            shapes: [
+                {
+                    type: 'line',
+                    yref: 'paper',
+                    x0: demoImput5,
+                    y0: 0,
+                    x1: demoImput5,
+                    y1: 1,
+                    line: {
+                        color: '#404040',
+                        width: 3
+                    }
+                }],
+
+
+        };
+        var config = { responsive: true }
+
+        Plotly.newPlot('demoChart5', data, layout, config);
+
+        // 6. MONTHLY INCOME PLOT
+        var trace = {
+            x: monInc,
+            type: 'box',
+            marker: { color: '#C14C8B' },
+            boxmean: 'sd'
+        };
+        var data = [trace];
+
+        var layout = {
+            title: {
+                text: "Monthly Income",
+                font: {
+            color: '#C14C8B'},
+                },
+            width: plotWidth,
+            height: plotHeight,
+            margin: {
+                l: 0,
+                r: 5,
                 b: 40,
                 t: 80,
                 pad: 0
@@ -306,40 +374,32 @@ function runEnter(demoImput1) {
             bargap: 0.2,
             xaxis: {
                 title: 'Income',
-                // linecolor: 'black',
-                // linewidth: 1,
-                // mirror: true
-            },
-            yaxis: {
-                // linecolor: 'black',
-                // linewidth: 1,
-                // mirror: true
             },
             annotations: [
                 {
-                font: {
-                    color: '#404040'
-                },
-                yref: 'paper',
-                x: demoImput6,
-                y: .8,
-                text: 'Employee Profile',
-                showarrow: true,
-                arrowhead: 7,
+                    font: {
+                        color: '#404040'
+                    },
+                    yref: 'paper',
+                    x: demoImput6,
+                    y: .8,
+                    text: 'Employee Profile',
+                    showarrow: true,
+                    arrowhead: 7,
                 }
             ],
             shapes: [
                 {
-                  type: 'line',
-                  yref: 'paper',
-                  x0: demoImput6,
-                  y0: 0,
-                  x1: demoImput6,
-                  y1: 1,
-                  line: {
-                    color: '#404040',
-                    width: 3
-                  }
+                    type: 'line',
+                    yref: 'paper',
+                    x0: demoImput6,
+                    y0: 0,
+                    x1: demoImput6,
+                    y1: 1,
+                    line: {
+                        color: '#404040',
+                        width: 3
+                    }
                 }],
 
 
