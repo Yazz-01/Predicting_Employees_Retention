c3313a63e66157566ce40a6a6b358713fb2f1e45
Fispit <sdelcos@gmail.com>
2021-08-27 15:00:25 -0500

    Fixed errors in the model call

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.core.getlimits import MachArLike
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
@@ -44,24 +45,62 @@ def smappage():
 @app.route("/predict/<Overtime>/<MaritalStatus>/<NumCompaniesWorked>/<TotalWorkingYears>/<InSales>/<Gender>/")
 def viajesdata(Overtime,MaritalStatus,NumCompaniesWorked,TotalWorkingYears,InSales,Gender):
     #note, InSales variable must be imported as 0 or 1
+    NumCompaniesWorked=int(NumCompaniesWorked)
+    TotalWorkingYears=int(TotalWorkingYears)
+    InSales=int(InSales)
     model=load('attrition_pred.lrm')
     scaler=load('lrm.scaler')   
-
+    print("Model and scaler Loading done")
 
     Fidelity = NumCompaniesWorked/TotalWorkingYears
-    data={'OverTime':[Overtime],
-    'Fidelity':[Fidelity],
-    'SalesDpt':[InSales],
-    'MaritalStatus':[MaritalStatus],
-    'Gender':[Gender]
-    }
+    # data={'OverTime':[Overtime],
+    # 'Fidelity':[Fidelity],
+    # 'SalesDpt':[InSales],
+    # 'MaritalStatus':[MaritalStatus],
+    # 'Gender':[Gender]
+    # }
+    male=0
+    female=0
+    OTyes=0
+    OTno=0
+    divorced=0
+    married=0
+    single=0
+
+    if MaritalStatus=="Single":
+        single=1
+    elif MaritalStatus=="Married":
+        married=1
+    else:
+        divorced=1
+
+    if Overtime=="Yes":
+        OTyes=1
+    else:
+        OTno=1
+
+    if Gender=="Male":
+        male=1
+    else:   
+        female=1
+    data={'Fidelity':[Fidelity],
+        'SalesDpt':[InSales],
+        'OverTime_No':[OTno],
+        'OverTime_Yes':[OTyes],
+        'MaritalStatus_Divorced':[divorced],
+        'MaritalStatus_Married':[married],
+        'MaritalStatus_Single':[single],
+        'Gender_Female':[female],
+        'Gender_Male':[male]}
+    print("Data insertion done")
     data=pd.DataFrame(data)
     data_binary_encoded = pd.get_dummies(data)
     X_scaled = scaler.transform(data_binary_encoded)
-
+    print("Scaling done")
     result=model.predict(X_scaled)
+    print("prediction done")
     print(result)
-    return result
+    return str(result[0])
 
 
 if __name__ == "__main__":
